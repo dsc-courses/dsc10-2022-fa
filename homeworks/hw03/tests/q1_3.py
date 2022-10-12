0a071bb16334a6465a13608ee5245a3d3d148e60
Gabriel Cha <gabrielcha@Gabriels-MacBook-Pro-2.local>
2022-10-11 23:39:09 -0700

    hw03 Released

---
+++ homeworks/hw03/tests/q1_3.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q1_3',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': ">>> isinstance(wester_river_north, bpd.DataFrame) and wester_river_north.shape[1] == 11 # Don't remove any columns!\nTrue",
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ homeworks/hw03/tests/q1_9.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_9',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> isinstance(badness_array, np.ndarray) # Make sure badness_array is an array, not a Series!\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
