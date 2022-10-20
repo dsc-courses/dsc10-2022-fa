ab97c0a9dfe7143f9c6bcf5d8291993fa617f298
Suraj Rampure <surajr@me.com>
2022-10-19 17:54:57 -0700

    midterm project released

---
+++ midterm_project/tests/q4_6.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q4_6',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': ">>> word_analysis('SeptEMBER', False).shape == (7, 3) and word_analysis('SeptEMBER', False).shape == (7, 3) # Make sure your function returns a "
+                                               'DataFrame\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
