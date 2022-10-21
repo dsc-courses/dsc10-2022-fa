43338927a76b0a821c8128877b6c8454b05102a9
Suraj Rampure <surajr@me.com>
2022-10-20 21:46:31 -0700

    update 7.1 tests

--- midterm_project/tests/q7_1.py
+++ midterm_project/tests/q7_1.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q7_1',
     'points': None,
-    'suites': [   {   'cases': [   {'code': '>>> isinstance(megahits, bpd.DataFrame) and megahits.shape[1] == 13\nTrue', 'hidden': False, 'locked': False},
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(megahits, bpd.DataFrame) and megahits.shape[1] == 15\nTrue', 'hidden': False, 'locked': False},
                                    {'code': ">>> 'danceability' in megahits.columns\nTrue", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
